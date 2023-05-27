22786049de4da666896a7f06c04eb0524688ae75
Imran Hossain <blur-imran@hotmail.com>
2023-05-27 19:05:09 +0600

    Update person.rb

--- person.rb
+++ person.rb
@@ -5,9 +5,9 @@ require_relative 'rental'
 
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age, :rentals
+  attr_accessor :name, :age, :rentals, :parent_permission
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name, parent_permission)
     super()
     @id = Random.rand(1..100)
     @name = name
@@ -34,10 +34,3 @@ class Person < Nameable
 
   private :of_age?
 end
-
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
