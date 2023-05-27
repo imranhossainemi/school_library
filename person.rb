67eee7eda54ff054e5f55d126bcbe1137d110783
Imran Hossain <blur-imran@hotmail.com>
2023-05-27 18:58:59 +0600

    Update person.rb

--- person.rb
+++ person.rb
@@ -1,10 +1,11 @@
 require_relative 'nameable'
 require_relative 'capitalize_decorator'
 require_relative 'trimmer_decorator'
+require_relative 'rental'
 
 class Person < Nameable
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
@@ -12,6 +13,11 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
+  end
+
+  def add_rental(date, book)
+    Rental.new(date, self, book)
   end
 
   def correct_name
@@ -21,6 +27,11 @@ class Person < Nameable
   def of_age?
     @age >= 18
   end
+
+  def can_use_services?
+    of_age? || @parent_permission
+  end
+
   private :of_age?
 end
 
